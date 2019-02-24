122156a8fa1e4e3f91b350ca4c85d2105434fab5
Jason <lookwhatjasondid@gmail.com>
2019-02-24 10:49:33 -0600

    add event listeners

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,18 @@
 /*
-  JavaScript serviceWorker v0.0.0
-  Copyright (c) 2019 Jason Niebauer
-  GitHub: https://github.com/JasonNiebauer/service-worker-template
-  License: http://www.opensource.org/licenses/mit-license.php
+    JavaScript serviceWorker v0.0.0
+    Copyright (c) 2019 Jason Niebauer
+    GitHub: https://github.com/JasonNiebauer/service-worker-template
+    License: http://www.opensource.org/licenses/mit-license.php
 */
+
+addEventListener('install', function(event){
+    console.log('The service worker is installing...');
+});
+
+addEventListener('activate', function(event){
+    console.log('The service worker is activated.');
+});
+
+addEventListener('fetch', function(event){
+    console.log('The service worker is listening.');
+});
