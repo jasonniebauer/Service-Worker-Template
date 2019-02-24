d39222d6d4ed19033c045870a8431d1a43e7ab0a
Jason Niebauer <jason.niebauer1@gmail.com>
2019-02-23 18:51:01 -0600

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,6 @@
+/*
+  JavaScript serviceWorker v0.0.0
+  Copyright (c) 2019 Jason Niebauer
+  GitHub: https://github.com/JasonNiebauer/service-worker-template
+  License: http://www.opensource.org/licenses/mit-license.php
+*/
