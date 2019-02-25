1ad370076d8a6557d7aaef4d879efef697082917
Jason <lookwhatjasondid@gmail.com>
2019-02-24 20:53:24 -0600

    update service worker

--- serviceworker.js
+++ serviceworker.js
@@ -13,6 +13,18 @@ addEventListener('activate', function(event){
     console.log('The service worker is activated.');
 });
 
-addEventListener('fetch', function(event){
-    console.log('The service worker is listening.');
-});
+addEventListener('fetch', fetchEvent => {
+    const request = fetchEvent.request;
+    fetchEvent.respondWith(
+        fetch(request)
+        .then( responseFromFetch => {
+            return responseFromFetch;
+        }) // end fetch then
+        .catch(error => {
+           return new Response('<h1>Oops!</h1> <p>Something went wrong.</p>',
+           {
+               headers: {'Content-type': 'text/html; charset=utf-8'}
+           }); 
+        }) // end fetch catch
+    ); // end respondWith
+}); // end addEventListener
