a861191d957db39730a57f549e792f53aa86fe12
Jason <lookwhatjasondid@gmail.com>
2019-02-27 21:37:16 -0600

    update service worker

--- serviceworker.js
+++ serviceworker.js
@@ -5,7 +5,8 @@
     License: http://www.opensource.org/licenses/mit-license.php
 */
 
-const staticCacheName = 'staticfiles';
+const version = 'V0.01';
+const staticCacheName = version + 'staticfiles';
 
 addEventListener('install', installEvent => {
     installEvent.waitUntil(
@@ -25,9 +26,23 @@ addEventListener('install', installEvent => {
     ); // end waitUntil
 }); // end addEventListener
 
-addEventListener('activate', function(event){
-    console.log('The service worker is activated.');
-});
+addEventListener('activate', activateEvent => {
+    activateEvent.waitUntil(
+        caches.keys()
+        .then( cacheNames => {
+            return Promise.all(
+                cacheNames.map( cacheName => {
+                    if (cacheName != staticCacheName) {
+                        return caches.delete(cacheName);
+                    } // end if
+                }) // end map
+            ); // end return Promise.all
+        }) // end keys then
+        .then( () => {
+            return clients.claim();
+        }) // end then
+    ); // end waitUntil
+}); // end addEventListener
 
 // When the browser requests a file...
 addEventListener('fetch', fetchEvent => {
